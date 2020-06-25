8d15b34fc7dc5d65cedd6c77ed54a879e54ed26b
antjocks <antjocks@gmail.com>
2020-06-24 23:19:01 -0700

    Switching to mysql in production

--- whattodo/settings.py
+++ whattodo/settings.py
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'whattodo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = mysql
 
 
 # Password validation
